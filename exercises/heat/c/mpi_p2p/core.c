e2542a928014112440cac83249babe318161f100
Daniel Cutting <dcfc20@sussex.ac.uk>
2017-07-04 15:32:06 +0300

    Trying to make hybrid heat exercise

--- exercises/heat/c/mpi_p2p/core.c
+++ exercises/heat/c/mpi_p2p/core.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <assert.h>
 #include <mpi.h>
-
+#include <omp.h>
 #include "heat.h"
 
 /* Exchange the boundary values */
@@ -23,7 +23,8 @@ void exchange(field *temperature, parallel_data *parallel)
   */
   //Non-blocking send and recv
   // Send up, tag is 0
-
+#pragma omp master
+  {
   MPI_Isend(temperature->data[1],temperature->ny,MPI_DOUBLE,parallel->nup,0,MPI_COMM_WORLD,&parallel->reqarray[sendup]);
   // Recv from down, tag is 0
   MPI_Irecv(temperature->data[temperature->nx+1],temperature->ny,MPI_DOUBLE,parallel->ndown,0,MPI_COMM_WORLD,&parallel->reqarray[recvdown]);
@@ -31,7 +32,7 @@ void exchange(field *temperature, parallel_data *parallel)
   MPI_Isend(temperature->data[temperature->nx],temperature->ny,MPI_DOUBLE,parallel->ndown,1,MPI_COMM_WORLD,&parallel->reqarray[senddown]);
   //Recv from up, tag is 1
   MPI_Irecv(temperature->data[0],temperature->ny,MPI_DOUBLE,parallel->nup,1,MPI_COMM_WORLD,&parallel->reqarray[recvup]);
-
+  }
   fflush(stdout);
   
 }
@@ -49,8 +50,30 @@ void evolve(field *curr, field *prev, double a, double dt,parallel_data *paralle
    * are not updated. 
    * We are only updating points that do not depend on completed sends/receives.
    */
+
+
   dx2 = prev->dx * prev->dx;
   dy2 = prev->dy * prev->dy;
+#pragma omp master
+  {
+    MPI_Waitall(4,parallel->reqarray,parallel->statarray);
+  }
+#pragma omp barrier
+  for (i = 1; i < curr->nx+1; i++) {
+    for (j = 1; j < curr->ny + 1; j++) {
+      curr->data[i][j] = prev->data[i][j] + a * dt *
+	((prev->data[i + 1][j] -
+	  2.0 * prev->data[i][j] +
+	  prev->data[i - 1][j]) / dx2 +
+	 (prev->data[i][j + 1] -
+	  2.0 * prev->data[i][j] +
+	  prev->data[i][j - 1]) / dy2);
+    }
+  }
+  
+
+    /*
+#pragma omp for private(i,j) 
   for (i = 3; i < curr->nx-1; i++) {
     for (j = 1; j < curr->ny + 1; j++) {
       curr->data[i][j] = prev->data[i][j] + a * dt *
@@ -64,25 +87,29 @@ void evolve(field *curr, field *prev, double a, double dt,parallel_data *paralle
   }
 
 
+#pragma omp master
+  {
 
   // Wait for the sends and recieves to complete.
-  MPI_Waitall(4,parallel->reqarray,parallel->statarray);
-
-
-  // Do the border points
-  int borderidx[4]={1,2,curr->nx-1,curr->nx};
-  for (int i=0;i<4;i++){
-    for(int j=1;j<curr->ny+1;j++){
-      curr->data[borderidx[i]][j] = prev->data[borderidx[i]][j] + a * dt *
-	((prev->data[borderidx[i] + 1][j] -
-	  2.0 * prev->data[borderidx[i]][j] +
-	  prev->data[borderidx[i] - 1][j]) / dx2 +
-	 (prev->data[borderidx[i]][j + 1] -
-	  2.0 * prev->data[borderidx[i]][j] +
-	  prev->data[borderidx[i]][j - 1]) / dy2);
-    }
-  }      
+    MPI_Waitall(4,parallel->reqarray,parallel->statarray);
   
+
+    // Do the border points
+    int borderidx[4]={1,2,curr->nx-1,curr->nx};
+    for (int i=0;i<4;i++){
+      for(int j=1;j<curr->ny+1;j++){
+	curr->data[borderidx[i]][j] = prev->data[borderidx[i]][j] + a * dt *
+	  ((prev->data[borderidx[i] + 1][j] -
+	    2.0 * prev->data[borderidx[i]][j] +
+	    prev->data[borderidx[i] - 1][j]) / dx2 +
+	   (prev->data[borderidx[i]][j + 1] -
+	    2.0 * prev->data[borderidx[i]][j] +
+	    prev->data[borderidx[i]][j - 1]) / dy2);
+      }
+    }
+
+  }
+    */
 }
 
 
--- exercises/heat/c/mpi_p2p/main.c
+++ exercises/heat/c/mpi_p2p/main.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <time.h>
 #include <mpi.h>
-
+#include <omp.h>
 #include "heat.h"
 
 
@@ -26,52 +26,64 @@ int main(int argc, char **argv)
     double dx2, dy2;            //!< delta x and y squared
 
     double start_clock;        //!< Time stamps
-
-
+    int provided;
 
     /* TODO start: initialize MPI */
-    MPI_Init(&argc,&argv);
+    MPI_Init_thread(&argc,&argv,MPI_THREAD_SERIALIZED,&provided);
     /* TODO end */
 
+    printf("Asked for %i, recieved %i \n",MPI_THREAD_SERIALIZED,provided);
 
-    initialize(argc, argv, &current, &previous, &nsteps, &parallelization);
+#pragma omp parallel private(iter) shared(current,previous)
+    {
+#pragma omp single
+      {    
 
-    /* Output the initial field */
-    write_field(&current, 0, &parallelization);
-    fflush(stdout);
-    /* Largest stable time step */
-    dx2 = current.dx * current.dx;
-    dy2 = current.dy * current.dy;
-    dt = (dx2 * dy2 / (2.0 * a * (dx2 + dy2)))/50.;
+	initialize(argc, argv, &current, &previous, &nsteps, &parallelization);
 
-    /* Get the start time stamp */
-    start_clock = MPI_Wtime();
-
-    /* Time evolve */
-    for (iter = 1; iter < nsteps; iter++) {
+	/* Output the initial field */
+	write_field(&current, 0, &parallelization);
+	fflush(stdout);
+	/* Largest stable time step */
+	dx2 = current.dx * current.dx;
+	dy2 = current.dy * current.dy;
+	dt = (dx2 * dy2 / (2.0 * a * (dx2 + dy2)))/50.;
+	
+	/* Get the start time stamp */
+	start_clock = MPI_Wtime();
+      }
+      /* Time evolve */
+      for (iter = 1; iter < nsteps; iter++) {
         exchange(&previous, &parallelization);
         evolve(&current, &previous, a, dt,&parallelization);
-        if (iter % image_interval == 0) {
-          write_field(&current, iter, &parallelization);
-	  if(parallelization.rank==0)	printf("Writing at step %i \n", iter);
-        }
-        /* Swap current field so that it will be used
-            as previous for next iteration step */
-        swap_fields(&current, &previous);
+#pragma omp master
+	{
+	  
+	  if (iter % image_interval == 0) {
+	    
+	    write_field(&current, iter, &parallelization);
+	    if(parallelization.rank==0)	printf("Writing at step %i \n", iter);
+	  }
+	  /* Swap current field so that it will be used
+	     as previous for next iteration step */
+	  swap_fields(&current, &previous);
+	}
+      }
+#pragma omp single
+      {
 
+	/* Determine the CPU time used for the iteration */
+	if (parallelization.rank == 0) {
+	  printf("Iteration took %.3f seconds.\n", (MPI_Wtime() - start_clock));
+	  printf("Reference value at 5,5: %f\n", previous.data[5][5]);
+	  
+	}
+	finalize(&current, &previous);
+      }
     }
-
-    /* Determine the CPU time used for the iteration */
-    if (parallelization.rank == 0) {
-      printf("Iteration took %.3f seconds.\n", (MPI_Wtime() - start_clock));
-      printf("Reference value at 5,5: %f\n", previous.data[5][5]);
-    }
-
-    finalize(&current, &previous);
-
     /* TODO start: finalize MPI */
     MPI_Finalize();
     /* TODO end */
-
+    
     return 0;
 }
