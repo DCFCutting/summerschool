af27711e8efd6ace88cbe7b6fae51ea223cb2edb
Daniel Cutting <dcfc20@sussex.ac.uk>
2017-07-04 15:35:59 +0300

    Forgot a file

--- exercises/mpi/c/hello-world/hello.c
+++ exercises/mpi/c/hello-world/hello.c
@@ -10,15 +10,16 @@ int main(int argc, char *argv[])
   int rank;
   MPI_Comm comm=MPI_COMM_WORLD;
   
-
+  required=MPI_THREAD_MULTIPLE;
   MPI_Init_thread(&argc,&argv,required,&provided);
-  prinf("Asked for support %i and recieved support %i",required,provided)
-
+  if(rank==0){
+    printf("Asked for support %i and recieved support %i \n",required,provided);
+  }
   MPI_Comm_size(comm,&size);
   
   
   MPI_Comm_rank(comm, &rank);
-  
+  fflush(stdout);
   
   if(rank==0){
     printf("There are %i processes in total. \n", size);
@@ -27,7 +28,7 @@ int main(int argc, char *argv[])
   {
     int tid,tnum;
     tid=omp_get_thread_num();
-    tnum=omp_get_num_thread();
+    tnum=omp_get_num_threads();
     printf("I am rank %i of %i, and thread %i of %i \n",rank,size-1,tid,tnum);  
   }
   MPI_Finalize();
