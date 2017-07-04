33e11521606cbb8f15da664a0672b438de936f5b
Daniel Cutting <dcfc20@sussex.ac.uk>
2017-07-04 14:15:28 +0300

    Hybrid on macs is dumb

--- exercises/mpi/c/hello-world/hello.c
+++ exercises/mpi/c/hello-world/hello.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <omp.h>
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-#pragma omp parallel
-    {
-        printf("X\n");
-    }
+  printf("Hello world!\n");
+  int size,required,provided;
+  int rank;
+  MPI_Comm comm=MPI_COMM_WORLD;
+  
 
-    return 0;
+  MPI_Init_thread(&argc,&argv,required,&provided);
+  prinf("Asked for support %i and recieved support %i",required,provided)
+
+  MPI_Comm_size(comm,&size);
+  
+  
+  MPI_Comm_rank(comm, &rank);
+  
+  
+  if(rank==0){
+    printf("There are %i processes in total. \n", size);
+  }
+#pragma omp parallel
+  {
+    int tid,tnum;
+    tid=omp_get_thread_num();
+    tnum=omp_get_num_thread();
+    printf("I am rank %i of %i, and thread %i of %i \n",rank,size-1,tid,tnum);  
+  }
+  MPI_Finalize();
+  return 0;
 }
